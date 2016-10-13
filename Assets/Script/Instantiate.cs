effb802043e7d6b51b996d7178135edbfdb76463
Luís Paulo Braga de Oliveira da Silva <lpaulobos2010@gmail.com>
2016-10-13 12:10:12 -0300

    Scene Grid Size
    Input size is not working

--- Assets/Script/Instantiate.cs
+++ Assets/Script/Instantiate.cs
@@ -6,9 +6,13 @@ public class Instantiate : MonoBehaviour {
 	public GameObject prefab;
 	// Use this for initialization
 	void Start () {
-		for (int i = 0; i < 64; i++) {
-			for (int j = 0; j < 32; j++){
-                Instantiate (prefab, new Vector2 (-8.29f + i * 1.2f, 4.39f - j* 1.2f), Quaternion.identity);
+		PlayerPrefs.SetInt ("rows", 13);
+		PlayerPrefs.SetInt("columns",6);
+
+		for (int i = 0; i < PlayerPrefs.GetInt("rows"); i++) {
+			for (int j = 0; j < PlayerPrefs.GetInt("columns"); j++){
+               GameObject grid =  Instantiate (prefab, new Vector2 (-8.29f + i * 1.2f, 4.39f - j* 1.2f), Quaternion.identity) as GameObject;
+				grid.name = "grid "+ (i.ToString() + j.ToString()).ToString(); 
 			}
 		}
 
---
+++ Assets/Script/SetGridSize.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class SetGridSize : MonoBehaviour {
+
+	public int a;
+	public int b;
+
+	
+	public void setGrid()
+	{
+		string f = GameObject.Find ("Text1").GetComponent<Text> ().text;
+		string e = GameObject.Find ("Text").GetComponent<Text> ().text;
+		a =  int.Parse (f);
+		b = int.Parse (e);
+		PlayerPrefs.SetInt ("rows",a);
+		PlayerPrefs.SetInt ("columns", b);
+	}
+}
